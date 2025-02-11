1969b802d7d0d149d70ba7059dba4a0935f0abf5
Anna <anna0983266723@gmail.com>
2025-02-11 10:36:15 +0800

    a

--- 55_3/Form1.Designer.cs
+++ 55_3/Form1.Designer.cs
@@ -118,9 +118,9 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(675, 302);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(33, 12);
+            this.label2.Size = new System.Drawing.Size(52, 12);
             this.label2.TabIndex = 7;
-            this.label2.Text = "label2";
+            this.label2.Text = "(360,300)";
             // 
             // Form1
             // 
